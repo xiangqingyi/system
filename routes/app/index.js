c27c4033976e9c0f2abe2b23accf8baaa8f7102c
xiangqingyi <1352493573@qq.com>
2018-11-07 17:48:58 +0800

    增加路由配置文件

---
+++ routes/app/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const api = require('../../controllers/api/v1/api');
+const app = require('../../controllers/app/user');
+const core = require('../../lib/core');
+
+router.use(function(req,res,next) {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers','X-Request-With,content-type,Authorization');
+    res.setHeader("Cache-Control","private,no-store");
+    next();
+})
+router.route('/login')
+  .get(app.login);
+router.route('/register')
+  .get(app.register)
+router.route('/')
+  .all(app.adminList);
+
+  module.exports = function(app) {
+      let path = core.translateHomePageDir('/');
+      app.use(path,router);
+  }
